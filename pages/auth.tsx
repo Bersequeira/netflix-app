ce125cf0cd8ef8c7b877006dee6b85496b0ac629
Bernardo <bersuarezsequeira@gmail.com>
2023-04-25 08:45:27 -0600

    icon

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -108,10 +108,10 @@ const Auth = () => {
             </button>
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
               <div onClick={() => signIn('google', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
-                <FcGoogle size={32} />
+               {/*  <FcGoogle size={32} /> */}
               </div>
               <div onClick={() => signIn('github', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
-                <FaGithub size={32} />
+              {/*   <FaGithub size={32} /> */}
               </div>
             </div>
             <p className="text-neutral-500 mt-12">
